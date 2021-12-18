cf09e29c06af13585143237792d54e3283b992fe
Bhumikareddynallamilli <96302064+Bhumikareddynallamilli@users.noreply.github.com>
2021-12-18 15:26:27 +0530

    Add files via upload

---
+++ minute.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int totalminutes,h,m;
+	scanf("%d",&totalminutes);
+	h=totalminutes/60;
+	m=totalminutes%60;
+	printf("%d hour(s) %d minute(s)",h,m);
+
+
+	
+	
+}
